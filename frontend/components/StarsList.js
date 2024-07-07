0d8b71e8949263d5f12172eb64cd336c933785dd
Ashley Lago <ashl3ylag0@gmail.com>
2024-07-07 15:46:47 -0400

    Finished Project

--- frontend/components/StarsList.js
+++ frontend/components/StarsList.js
@@ -1,10 +1,39 @@
 import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 export default function StarsList() {
+  const navigate = useNavigate()
   const [stars, setStars] = useState([])
+
+  const handleLogout = () => {
+    localStorage.removeItem('token')
+    navigate('/')
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      const fetchStars = async () => {
+        try {
+          const { data } = await axios.get(
+            '/api/stars',
+            { headers: { Authorization: token } }
+          )
+          setStars(data)
+        } catch (err) {
+          if (err.response.status == 401) {
+            handleLogout()
+          }
+        }
+      }
+      fetchStars()
+    } else { navigate('/') }
+  }, [])
+
   return (
     <div className="container">
-      <h3>StarsList <button>Logout</button></h3>
+      <h3>StarsList <button onClick={() => handleLogout()}>Logout</button></h3>
       {stars.length > 0 ? (
         <div>
           {stars.map((star) => (
